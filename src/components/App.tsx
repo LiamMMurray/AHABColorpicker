35daf80957f44522b0478282cb004b49512b2596
Liam <lmmurray@student.fullsail.edu>
2020-04-10 17:24:07 -0400

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const App = () => {
+  return <div id="App"></div>;
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module:
+    {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.svg$/,
+                use: ['svg-inline-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias:
+        {
+            "@components": path.resolve(__dirname, './src/components'),
+            "@math": path.resolve(__dirname, './src/math'),
+            "@images": path.resolve(__dirname, './src/images'),
+            "@styles": path.resolve(__dirname, './src/styles'),
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ]
+}
